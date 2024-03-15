81bbdbca91d289c2dc2f63875bc9a57082f0c149
flex18 <emendozaminchan@gmail.com>
2024-03-15 13:53:19 -0500

    add structure yanki ms

---
+++ src/test/java/com/nttdata/yanki/management/sm/YankiManagementSmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.yanki.management.sm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YankiManagementSmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
