8298ee7c3d55256083b17a67dbdd7500264bda5d
flex18 <emendozaminchan@gmail.com>
2024-03-15 19:36:29 -0500

    git add change in class names

--- src/main/java/com/nttdata/yanki/management/sm/model/DebitCardAssociationEvent.java
+++ src/main/java/com/nttdata/yanki/management/sm/model/DebitCardAssociationEvent.java
@@ -1,24 +1,18 @@
 package com.nttdata.yanki.management.sm.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import java.io.Serializable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class User implements Serializable {
+public class DebitCardAssociationEvent {
 
-  @Id
-  private String userId;
-  private String dni;
   private String cellPhoneNumber;
-  private String imeiCellPhone;
-  private String email;
+  private String debitCardNumber;
 }
--- src/main/java/com/nttdata/yanki/management/sm/model/UserRegisterEvent.java
+++ src/main/java/com/nttdata/yanki/management/sm/model/UserRegisterEvent.java
@@ -1,18 +1,24 @@
 package com.nttdata.yanki.management.sm.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import java.io.Serializable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class DebitCardAssociation {
+public class UserRegisterEvent implements Serializable {
 
+  @Id
+  private String userId;
+  private String dni;
   private String cellPhoneNumber;
-  private String debitCardNumber;
+  private String imeiCellPhone;
+  private String email;
 }
