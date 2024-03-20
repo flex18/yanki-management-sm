1a52f660639b4eba8c73d3194cb62954833b9cfe
flex18 <emendozaminchan@gmail.com>
2024-03-20 18:35:25 -0500

    add send-payment

--- src/main/java/com/nttdata/yanki/management/sm/repository/PaymentEventRepository.java
+++ src/main/java/com/nttdata/yanki/management/sm/repository/PaymentEventRepository.java
@@ -1,6 +1,7 @@
 package com.nttdata.yanki.management.sm.repository;
 
+import com.nttdata.yanki.management.sm.model.PaymentMadeEvent;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
-public interface PaymentEventRepository extends ReactiveMongoRepository {
+public interface PaymentEventRepository extends ReactiveMongoRepository<PaymentMadeEvent, String> {
 }
